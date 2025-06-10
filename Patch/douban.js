052c99df5fa8f88df71f728b0799eb9dc208d456
Purkylin <purkylin@gmail.com>
2025-06-10 11:25:28 +0800

    update douban

--- Patch/douban.js
+++ Patch/douban.js
@@ -1,38 +1,122 @@
 const ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)'
-const tabs = [
+const tv = [
     {
-        "title": "国产剧",
-        "ext": "tv_domestic",
+        "category": "tv",
+        "type": "tv",
+        "title": "综合"
     },
     {
-        "title": "韩国剧",
-        "ext": "tv_korean",
+        "category": "tv",
+        "type": "tv_domestic",
+        "title": "国产剧"
     },
     {
-        "title": "欧美剧",
-        "ext": "tv_american",
+        "category": "show",
+        "type": "show",
+        "title": "综艺"
     },
     {
-        "title": "动画",
-        "ext": "tv_animation",
+        "category": "tv",
+        "type": "tv_american",
+        "title": "欧美剧"
     },
     {
-        "title": "影院热映",
-        "ext": "movie_showing",
+        "category": "tv",
+        "type": "tv_japanese",
+        "title": "日剧"
     },
     {
-        "title": "豆瓣热门",
-        "ext": "movie_hot_gaia",
+        "category": "tv",
+        "type": "tv_korean",
+        "title": "韩剧"
+    },
+    {
+        "category": "tv",
+        "type": "tv_animation",
+        "title": "动画"
+    },
+    {
+        "category": "tv",
+        "type": "tv_documentary",
+        "title": "纪录片"
     }
 ]
-
+const movie = [
+    {
+        "category": "热门",
+        "type": "全部",
+        "title": "热门"
+    },
+    {
+        "category": "最新",
+        "type": "全部",
+        "title": "最新"
+    },
+    {
+        "category": "豆瓣高分",
+        "type": "全部",
+        "title": "豆瓣高分"
+    },
+    {
+        "category": "冷门佳片",
+        "type": "全部",
+        "title": "冷门佳片"
+    },
+    {
+        "category": "热门",
+        "type": "华语",
+        "title": "华语"
+    },
+    {
+        "category": "热门",
+        "type": "欧美",
+        "title": "欧美"
+    },
+    {
+        "category": "热门",
+        "type": "韩国",
+        "title": "韩国"
+    },
+    {
+        "category": "热门",
+        "type": "日本",
+        "title": "日本"
+    }
+]
 async function getTabs() {
-    return tabs;
+    const tvItems = tv.map(x => ({
+        title: x.title,
+        ext: JSON.stringify({
+            type: x.type,
+            category: x.category,
+            tag: "tv"
+        }),
+    }));
+    const movieItems = movie.map(x => ({
+        title: x.title,
+        ext: JSON.stringify({
+            type: x.type,
+            category: x.category,
+            tag: "movie"
+        }),
+    }));
+
+    return [
+        { "title": "电影", "items": movieItems },
+        { "title": "电视剧", "items": tvItems },
+    ];
 }
 
 async function getCards(ext) {
-    const url = `https://m.douban.com/rexxar/api/v2/subject_collection/${ext}/items`
-    const referer = 'https://m.douban.com/tv/'
+    const { tag, type, category } = JSON.parse(ext);
+    const params = new URLSearchParams({
+        limit: "50",
+        type: type,
+        category: category,
+    });
+    const referer = 'https://movie.douban.com/';
+    const path = tag === 'movie' ? 'movie' : 'tv';
+    const url = `https://m.douban.com/rexxar/api/v2/subject/recent_hot/${path}?${params.toString()}`;
     const { data } = await $fetch.get(url, {
         headers: {
             'User-Agent': ua,
@@ -40,16 +124,14 @@ async function getCards(ext) {
         }
     })
     const result = JSON.parse(data)
-    const cards = result.subject_collection_items.map(x => ({
+    const cards = result.items.map(x => ({
         id: x.id,
         title: x.title,
         year: x.year,
         rating: x.rating?.value,
-        poster: x.pic?.normal || x.cover?.url,
-        status: x.episodes_info,
-        backdrop: x.photos?.[0]?.url,
-        tags: x.card_subtitle,
-        url: `https://movie.douban.com/subject/${x.id}/`
-    }))
+        poster: x.pic.large,
+        status: x.episodes_info
+    }));
+
     return cards
 }

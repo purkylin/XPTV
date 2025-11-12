e95bd547abc7a801ca3969d0c07ce273b0be1001
Purkylin <purkylin@gmail.com>
2025-11-12 18:08:54 +0800

    fix duboku

--- js/duboku.js
+++ js/duboku.js
@@ -2,8 +2,8 @@
 const cheerio = createCheerio()
 const UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"
 const headers = {
-  'Referer': 'https://tv.gboku.com/',
-  'Origin': 'https://tv.gboku.com',
+  'Referer': 'https://duboku.info/',
+  'Origin': 'https://duboku.info',
   'User-Agent': UA,
 }
 
